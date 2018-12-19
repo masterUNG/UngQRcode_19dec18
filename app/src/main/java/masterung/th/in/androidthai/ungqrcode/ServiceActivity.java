327e163952936a2817657fbdc325bb3a2e4308a9
masterUNG <phrombutr@gmail.com>
2018-12-19 15:45:19 +0700

    Log Out Success

--- app/src/main/java/masterung/th/in/androidthai/ungqrcode/ServiceActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/ungqrcode/ServiceActivity.java
@@ -1,13 +1,63 @@
 package masterung.th.in.androidthai.ungqrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //    Explicit
+    private String nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Find NameLogin
+        findNameLogin();
+
+//        Create Toolbar
+        createToolbar();
+
+    }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemLogOut) {
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signOut();
+            startActivity(new Intent(ServiceActivity.this, MainActivity.class));
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void findNameLogin() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        nameString = firebaseUser.getDisplayName();
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setSubtitle(nameString);
+    }
+
+}   // Main Class
