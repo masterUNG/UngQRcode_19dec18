b5470dd079f72855d1e79edb0b20757162e95912
masterUNG <phrombutr@gmail.com>
2018-12-19 15:58:41 +0700

    Finish 19dec

--- app/src/main/java/masterung/th/in/androidthai/ungqrcode/MainFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/ungqrcode/MainFragment.java
@@ -3,13 +3,19 @@ package masterung.th.in.androidthai.ungqrcode;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -33,8 +39,48 @@ public class MainFragment extends Fragment {
 //        Check Login
         checkLogin();
 
+//        Login Controller
+        loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                final MyAlert myAlert = new MyAlert(getActivity());
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    myAlert.normalDialog("Have Space", "Please Fill All Blank");
+                } else {
+
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(getActivity(), ServiceActivity.class));
+                                getActivity().finish();
+                            } else {
+                                myAlert.normalDialog("Login False", task.getException().toString());
+                            }
+                        }
+                    });
+
+
+                }
+
+
+            }
+        });
+    }
 
     private void checkLogin() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
